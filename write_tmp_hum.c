79abef3cc20b3d2219b95af5ab07f136e7d306e3
Xiang Jin <xiang01nij@gmail.com>
2025-04-29 09:47:49 -0400

    Added GPIO relay support, integrated with startup shell script

--- write_tmp_hum.c
+++ write_tmp_hum.c
@@ -15,48 +15,14 @@
 
 
 // Macros 
-#define LIGHT_GPIO 			66			// GPIO number used for controlling flood light relay
-#define TEMP_THRESHOLD		27			// Threshold temperature (in Celsius) below which the grow lights should turn on
+#define LIGHT_GPIO 			66			    // GPIO number used for controlling flood light relay
+#define TEMP_THRESHOLD		27			    // Threshold temperature (in Celsius) below which the grow lights should turn on
 
 
-
-int read_adc_raw(int channel) {
-    char path[64];
-    char buf[16];
-    int fd, len;
-
-    // build the sysfs path for in_voltage<channel>_raw
-    snprintf(path, sizeof(path),
-             "/sys/bus/iio/devices/iio:device0/in_voltage%d_raw",
-             channel);
-
-    // open for read
-    fd = open(path, O_RDONLY);
-    if (fd < 0) {
-        perror("open adc raw");
-        return -1;
-    }
-
-    // read the ASCII number
-    len = read(fd, buf, sizeof(buf)-1);
-    close(fd);
-    if (len <= 0) {
-        perror("read adc raw");
-        return -1;
-    }
-    buf[len] = '\0';
-
-    return atoi(buf);
-}
-
-
-double raw_to_degree(int raw_value){
-    double voltage= 1.8 * (double) raw_value /4096;
-    double tmp = (voltage-0.5)/10;
-    return tmp;
-}
-
-
+// Function declarations
+int read_adc_raw (int channel);             // Taking ADC data and outputting digital reading of the voltage for temperature sensors
+double raw_to_degree (int raw_value);       // Raw digital reading to degrees
+void digitalWrite (int gpio, int value);    // Write HIGH or LOW to a selected GPIO pin (doesn't ensure GPIO pin is set up!!)
 
 
 
@@ -140,7 +106,7 @@ int main()
 
 	// Read 2 bytes of temperature data
 	// temp msb, temp lsb
-	if(read(file, data, 2) != 2)
+	if (read(file, data, 2) != 2)
 	{
 		printf("Error : Input/output Error \n");
 	}
@@ -175,82 +141,154 @@ int main()
 	// If the temperature falls below 27 degrees C, then the grow lights should come on
 	if (degree < TEMP_THRESHOLD) {
 
-		
-	}
+		digitalWrite(LIGHT_GPIO, 1);
+	} else {
 
-	return 0;
+        digitalWrite(LIGHT_GPIO, 0);
+    }
 
+    while (1) {
 
-    while (1){
-        // Get I2C device, SI7021 I2C address is 0x40(64)
-
-        ioctl(file, I2C_SLAVE, 0x40);
-
-        // Send humidity measurement command(0xF5)
-        char config[1] = {0xF5};
-        write(file, config, 1);
         sleep(1);
-        float humidity;
-        // Read 2 bytes of humidity data
-        // humidity msb, humidity lsb
-        char data[2] = {0};
-        if(read(file, data, 2) != 2)
-        {
-            printf("Error : Input/output Error \n");
-        }
-        else
-        {
-            // Convert the data
-            humidity = (((data[0] * 256 + data[1]) * 125.0) / 65536.0) - 6;
-
-            // Output data to screen
-        }
-
-        // Send temperature measurement command(0xF3)
-        config[0] = 0xF3;
-        write(file, config, 1); 
+        digitalWrite(LIGHT_GPIO, 1);
         sleep(1);
+        digitalWrite(LIGHT_GPIO, 0);
+    }
 
-        // Read 2 bytes of temperature data
-        // temp msb, temp lsb
-        FILE* fp;
-        if((fp= fopen ("output.txt","w"))<0){
-            printf("Failed to open output.txt");
-        }
+    // while (1) {
+    //     // Get I2C device, SI7021 I2C address is 0x40(64)
+
+    //     ioctl(file, I2C_SLAVE, 0x40);
+
+    //     // Send humidity measurement command(0xF5)
+    //     char config[1] = {0xF5};
+    //     write(file, config, 1);
+    //     sleep(1);
+    //     float humidity;
+    //     // Read 2 bytes of humidity data
+    //     // humidity msb, humidity lsb
+    //     char data[2] = {0};
+    //     if(read(file, data, 2) != 2)
+    //     {
+    //         printf("Error : Input/output Error \n");
+    //     }
+    //     else
+    //     {
+    //         // Convert the data
+    //         humidity = (((data[0] * 256 + data[1]) * 125.0) / 65536.0) - 6;
+
+    //         // Output data to screen
+    //     }
+
+    //     // Send temperature measurement command(0xF3)
+    //     config[0] = 0xF3;
+    //     write(file, config, 1); 
+    //     sleep(1);
+
+    //     // Read 2 bytes of temperature data
+    //     // temp msb, temp lsb
+    //     FILE* fp;
+    //     if((fp= fopen ("output.txt","w"))<0){
+    //         printf("Failed to open output.txt");
+    //     }
         
-        if(read(file, data, 2) != 2)
-        {
-            printf("Error : Input/output Error \n");
-        }
-        else
-        {
-            // Convert the data
-            float cTemp = (((data[0] * 256 + data[1]) * 175.72) / 65536.0) - 46.85;
-            float fTemp = cTemp * 1.8 + 32;
+    //     if(read(file, data, 2) != 2)
+    //     {
+    //         printf("Error : Input/output Error \n");
+    //     }
+    //     else
+    //     {
+    //         // Convert the data
+    //         float cTemp = (((data[0] * 256 + data[1]) * 175.72) / 65536.0) - 46.85;
+    //         float fTemp = cTemp * 1.8 + 32;
 
-            // Output data to screen
-            fprintf(fp,"Temperature: %.2f C \n", cTemp);
-        }
+    //         // Output data to screen
+    //         fprintf(fp,"Temperature: %.2f C \n", cTemp);
+    //     }
 
 
-        fprintf(fp,"Relative Humidity : %.2f RH \n", humidity);
+    //     fprintf(fp,"Relative Humidity : %.2f RH \n", humidity);
 
-        for (int channel=0 ; channel<3;channel++){
-            double degree;
-            int raw = read_adc_raw(channel);
-            degree = raw_to_degree(raw);
-            if (raw < 0) {
-                fprintf(stderr, "Failed to read ADC channel %d\n", channel);
-                return 1;
-            }
-            fprintf(fp,"Temperature: %.2f C \n", degree);   
-        }
+    //     for (int channel=0 ; channel<3;channel++){
+    //         double degree;
+    //         int raw = read_adc_raw(channel);
+    //         degree = raw_to_degree(raw);
+    //         if (raw < 0) {
+    //             fprintf(stderr, "Failed to read ADC channel %d\n", channel);
+    //             return 1;
+    //         }
+    //         fprintf(fp,"Temperature: %.2f C \n", degree);   
+    //     }
         
-        fclose(fp);
-        sleep(1);
-    }   
+    //     fclose(fp);
+    //     sleep(1);
+    // }   
+
+    return 0;
 }
 
 
 
+/***************************** Function Implementatios *********************************/
 
+// Taking ADC data and outputting digital reading of the voltage for temperature sensors
+int read_adc_raw(int channel) {
+    char path[64];
+    char buf[16];
+    int fd, len;
+
+    // build the sysfs path for in_voltage<channel>_raw
+    snprintf(path, sizeof(path),
+             "/sys/bus/iio/devices/iio:device0/in_voltage%d_raw",
+             channel);
+
+    // open for read
+    fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        perror("open adc raw");
+        return -1;
+    }
+
+    // read the ASCII number
+    len = read(fd, buf, sizeof(buf)-1);
+    close(fd);
+    if (len <= 0) {
+        perror("read adc raw");
+        return -1;
+    }
+    buf[len] = '\0';
+
+    return atoi(buf);
+}
+
+
+
+// Raw digital reading to degrees
+double raw_to_degree(int raw_value){
+    double voltage= 1.8 * (double) raw_value /4096;
+    double tmp = (voltage-0.5)/10;
+    return tmp;
+}
+
+
+
+// Write HIGH or LOW to a selected GPIO pin (doesn't ensure GPIO pin is set up!!)
+void digitalWrite(int gpio, int value) {
+    char path[64];
+    int fd;
+
+    // Set GPIO value
+    snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/value", LIGHT_GPIO);
+    fd = open(path, O_WRONLY);
+    if (fd < 0) {
+        perror("Failed to open gpio value");
+        return;
+    }
+
+    if (value)
+        write(fd, "1", 2);
+    else
+        write(fd, "0", 2);
+
+    close(fd);
+}
